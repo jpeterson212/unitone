0aa7bb35da30ee5a0c4fb37fb40772c991030eb8
jpeterson212 <60198477+jpeterson212@users.noreply.github.com>
2020-02-04 21:16:50 -0600

    version_3

--- js/main.js
+++ js/main.js
@@ -41,7 +41,55 @@ function cities(){
         //add the row's html string to the table
         $("table").append(rowHtml);
     };
-};
+};    //Added at Example 3.5 line 44...
+    //get the div id
+    var theid = $('#mydiv').attr('id');
+
+    //theid is 'mydiv'; add it as text to the div
+    $('#mydiv').append(theid);
+
+    //add the class 'foo' to the div
+    $('#mydiv').attr('class', 'foo');
+
+    //Check your work with the Inspector!
+    //Added below Example 3.6...
+    //change the text color
+    $('#mydiv').css('color', 'red');
+
+    //change the text size and alignment
+    $('#mydiv').css({
+        'font-size': '2em',
+        'text-align': 'left'
+    });
+
+    //get the text color and add it as text to the div
+    var thecolor = $('#mydiv').css('color');
+    $('#mydiv').append(thecolor);
+
+    //fooled ya! thecolor is rgb(255, 0, 0), the CSS interpreter's translation of the keyword 'red'
+
+    //Added below Example 3.8...
+    //click listener with anonymous handler function
+    $('table').on('click', function(){
+        alert('Madison Rocks! Go Badgers!');
+    });
+    
+    //alias method for the click event listener
+    $('table').click(function(){
+        alert('Visit Superior and see the big lake!');
+    });
+
+    //named handler function for removable listener
+    function clickme(){
+        alert('Yeah Green Bay! Go Packers!');
+    };
+
+    //add the event listener
+    $('table').on('click', clickme);
+
+    //remove the event listener
+    $('table').off('click', clickme);
+
 
 //call the initialize function when the document has loaded
 $(document).ready(initialize);
